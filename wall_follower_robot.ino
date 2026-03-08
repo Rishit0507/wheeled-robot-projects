9cb31bd7cf7884180786b25bd770318b0b9d5b1e
Rishit_Kanjani <rishitkanjani123@gmail.com>
2026-03-08 19:18:59 +0530

    Add files via upload

---
+++ wall_follower_robot.ino
@@ -0,0 +1,98 @@
+int lmt1 = 5;
+int lmt2 = 3;
+int rmt1 = 6;
+int rmt2 = 11;
+int ls = 7;
+int cs = 8;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(lmt1,OUTPUT);
+pinMode(lmt2,OUTPUT);
+pinMode(rmt1,OUTPUT);
+pinMode(rmt2,OUTPUT);
+pinMode(ls,INPUT);
+pinMode(cs,INPUT);
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly
+ls = digitalRead(7);
+cs = digitalRead(8);
+
+if(ls==LOW && cs==LOW)
+{
+  stpleft();
+}
+else if(ls==HIGH && cs==LOW)
+{
+  stpright();
+}
+else if(ls==LOW && cs==HIGH)
+{
+  right();
+  delay(300);
+}
+else if(ls==HIGH && cs==HIGH)
+{ 
+  right();
+  delay(300);
+}
+ 
+}
+void forward()
+{
+analogWrite(lmt1,150);
+analogWrite(lmt2,0);
+analogWrite(rmt1,150);
+analogWrite(rmt2,0);
+}
+
+void reverse()
+{
+analogWrite(lmt1,0);
+analogWrite(lmt2,150);
+analogWrite(rmt1,0);
+analogWrite(rmt2,150);
+}
+
+void left()
+{
+analogWrite(lmt1,0);
+analogWrite(lmt2,150);
+analogWrite(rmt1,150);
+analogWrite(rmt2,0);
+}
+
+void right()
+{
+analogWrite(lmt1,150);
+analogWrite(lmt2,0);
+analogWrite(rmt1,0);
+analogWrite(rmt2,150);
+}
+
+void stpleft()
+{
+analogWrite(lmt1,0);
+analogWrite(lmt2,0);
+analogWrite(rmt1,150);
+analogWrite(rmt2,0);  
+}
+
+void stpright()
+{
+analogWrite(lmt1,150);
+analogWrite(lmt2,0);
+analogWrite(rmt1,0);
+analogWrite(rmt2,0);  
+}
+
+void stp()
+{
+analogWrite(lmt1,0);
+analogWrite(lmt2,0);
+analogWrite(rmt1,0);
+analogWrite(rmt2,0);
+}
